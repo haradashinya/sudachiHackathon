26af8f23723c14ead07087c50989c27a1284b140
haradashinya <haradashinya@gmail.com>
2012-11-10 17:25:16 +0900

    download files

--- download.js
+++ download.js
@@ -1,7 +1,12 @@
-//function download() {
-//    $(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").each(function() {
-//        alert($(this).attr("href"));
-//    });
-//}
+$(document).ready(function() {
+  function download() {
+    var self = this;
+    console.log($("body"));
+    $(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").each(function(item) {
+       console.log($(this).attr("href"));
+    }, this);
+  }
 
-console.log("hello world");
+  download();
+});
+
