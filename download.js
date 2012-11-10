4112a67001faa3a143d728c937095f9642844b28
haradashinya <haradashinya@gmail.com>
2012-11-10 20:28:57 +0900

    デバッグ用のモードを追加

--- download.js
+++ download.js
@@ -2,20 +2,45 @@
 
 
 $(document).ready(function() {
-	console.log("helllo world");
 	var writer = new window.Writer();
-	
+	console.log(writer);
+	var links = [];
 
 	function download() {
 		var self = this;
-		$(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").each(function(item) {
-				 // writer.writeToFile(item);
+		var isJQuery;
 
-			// if ($(this).attr("href"))
-			// 	targetLinks.push($(this).attr("href")
-			// 	);
-		}, this);
+		console.log(!!$(".dataList").html());
+		if(!!$(".dataList").html()){
+
+			isJQuery = true;
+
+			// call $(this).attr("href");
+			$(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").each(function(item) {
+				writer.writeToFile($(this).attr("href"));
+				
+			},this);
+			// go to  call jquery 
+			console.log("hello world");
+		}else{
+			links = ["<a href='https://github.com/okamurayasuyuki/sudachiHackathon/archive/master.zip'>this is a sample link</a>"];
+
+
+			links.map(function(link){
+				var linkStr = $(link).attr("href");
+				writer.writeToFile(linkStr);
+			},this);
+
+			console.log("Else");
+
+		}
+
 	}
+
+
+
+
+
 
 	download();
 });
