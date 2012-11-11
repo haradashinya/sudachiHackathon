5d76fb396b8d997df3092510ecf6d9c6dc04f74c
ramusara <ramusara@gmail.com>
2012-11-11 10:49:47 +0900

    refactoring

--- download.js
+++ download.js
@@ -1,54 +1,79 @@
 $(document).ready(function() {
    var downloadCount = 0;
+   var folderCount = 0;
    var writer = new window.Writer();
-   var links = [];
+   var fileLinks = [];
+   var folderLinks = [];
 
    function download(callback) {
       var self = this;
-      var isJQuery;
 
       // folder
-      console.log(!!$(".dataList").html());
-      if($(".vr_mainColumn").children("div:nth-child(4)").html() === "\nサブフォルダなし\n") {
-         // nothing
+      var folderLink = $(".vr_mainColumn").children("div:nth-child(4)");
+      if(folderLink.html() !== "\nサブフォルダなし\n") {
+         folderLink.find("a").each(function(item) {
+            console.log("folder=" + this);
+            folderLinks.push(this);
+         }, this);
+         folderCount = folderLink.find("a").length;
+      }
+
+      // file
+      if(!!$(".dataList").html()){
+         var fileLink = $(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a")
+         fileLink.each(function(item) {
+            console.log("file=" + this);
+            fileLinks.push(this);
+         }, this);
+         downloadCount = fileLink.length;
+
+         callback();
+      }
+   }
+
+   var isFileLinkExist = function() {
+      if (fileLinks.length !== 0) {
+         return true;
       } else {
-         $(".vr_mainColumn").children("div:nth-child(4)").find("a").each(function(item) {
-            console.log(this);
-            $("body").bind("click", function() {
-               console.log(window.location.hash);
-               window.location.hash = "hoge";
-            });
-            window.open(this);
+         return false;
+      }
+   }
+
+   var downloadFile = function() {
+      var filelink = fileLinks.pop();
+      console.log("download(" + filelink + ")");
+      writer.writeToFile(filelink);
+   }
+
+   var openNextFolder = function() {
+      if (folderLinks.length !== 0) {
+         var i = 0;
+         folderLinks.forEach(function(folderLink) {
+            setTimeout(function() {
+               console.log("open folderLink=" + folderLink);
+               window.open(folderLink);
+
+               folderCount--;
+               if (folderCount <= 0) {
+                  window.open('', '_self', '');
+                  window.close();
+               }
+            }, i * 3000);
+            i++;
          }, this);
       }
-
-      // data
-      if(!!$(".dataList").html()){
-         isJQuery = true;
-
-         // call $(this).attr("href");
-         var i = 1;
-         downloadCount = $(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").length;
-         $(".dataList").find("tbody").children("tr:nth-child(1)").nextAll().children("td:nth-child(2)").find("a").each(function(item) {
-            var downloadLink = this;
-            setTimeout(function() {
-               writer.writeToFile(downloadLink);
-               callback();
-            }, i * 500);
-            i++;
-            console.log("loop");
-         },this);
-         // go to  call jquery 
-         console.log("hello world");
-      }
    }
 
-   var onFinishedDownload = function() {
-      downloadCount--;
-      if (downloadCount === 0) {
-         window.open('', '_self', '');
-         window.close();
+   var onDownload = function() {
+      if (isFileLinkExist()) {
+         downloadFile();
+         onDownload();
+         downloadCount--;
+      }
+
+      if (downloadCount <= 0) {
+         openNextFolder();
       }
    }
-   download(onFinishedDownload);
+   download(onDownload);
 });
--- writer.js
+++ writer.js
@@ -2,7 +2,6 @@ window.Writer = function(){
 	this.writeToFile = function(link){
 		console.log("called writer");
 		console.log(link);
-
         window.open(link);
 	};
 };
